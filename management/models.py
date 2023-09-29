eea08045ed93d36a4283af1cf188fef2083b01ef
Siddhartha45 <87509046+Siddhartha45@users.noreply.github.com>
2023-09-29 17:35:18 +0545

    created models related to menu and food

--- management/models.py
+++ management/models.py
@@ -1,20 +1,41 @@
 from django.db import models
 
 
-class MenuCategory(models.Model):
-    name = models.CharField(max_length=150)
-    
-    
-class FoodItem(models.Model):
-    name = models.CharField(max_length=150)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    
-    
 class FoodCategory(models.Model):
-    name = models.CharField(max_length=150)
-    description = models.TextField()
-    menu_category = models.ForeignKey('MenuCategory')
+    """
+    Model for different food categories.
+    different types of roti are in roti items category.
+    ex: 'roti items' contains butter roti, naan roti etc.
+    """
+    name = models.CharField(max_length=80)
     
+    def __str__(self):
+        return self.name
+
+
+class MenuCategory(models.Model):
+    """
+    Model for associating food items with different menus like 
+    Dining Menu ,Delivery Menu etc.
+    """
+    name = models.CharField(max_length=100)
     
-class MenuHaveFood(models.Model):
-    menu_category = models.ForeignKey('MenuCategory')
+    def __str__(self):
+        return self.name
+
+
+class FoodItem(models.Model):
+    """
+    Model for particular food items. 
+    ex: chicken momo, butter roti.
+    """
+    name = models.CharField(max_length=80)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True)
+    food_category = models.ForeignKey(
+        'FoodCategory', on_delete=models.SET_NULL, null=True
+    )
+    menu_category = models.ManyToManyField('MenuCategory')
+    
+    def __str__(self):
+        return self.name
