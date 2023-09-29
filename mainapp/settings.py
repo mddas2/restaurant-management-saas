dec94d85db55cd1223830d9973faff69d83f267f
Sgrneane <sgrneaneprof@gmail.com>
2023-09-29 16:04:58 +0545

    aa

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -78,14 +78,22 @@ WSGI_APPLICATION = 'mainapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'restro',
+#         'USER': 'postgres',
+#         'PASSWORD': '12345',
+#         'PORT': '5432'
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'restro',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'PORT': '5432'
-    }
+'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
+}
 }
 
 
