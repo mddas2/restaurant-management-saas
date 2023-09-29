b43c6281703f7320f2b556f2f5ea3cd2c79c2538
Siddhartha45 <87509046+Siddhartha45@users.noreply.github.com>
2023-09-29 15:46:25 +0545

    aa

--- management/models.py
+++ management/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=150)
+    
+    
+class FoodItem(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    
+class FoodCategory(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    menu_category = models.ForeignKey('MenuCategory')
+    
+    
+class MenuHaveFood(models.Model):
+    menu_category = models.ForeignKey('MenuCategory')
