ab75afa4bdb51294eb9c1721d36fdcf036dbce38
Sgrneane <sgrneaneprof@gmail.com>
2023-09-29 15:38:10 +0545

    Database Configured to postgres

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'mainapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'restro',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'PORT': '5432'
     }
 }
 
@@ -126,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL='account.CustomUser'
