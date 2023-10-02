d7db5c589d69f5f90a3bc466949c50a7c65a3e18
Sheetal Sharma <sheetalsharma0719@gmail.com>
2023-10-02 16:39:27 +0545

    Configured : sqlite3

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -80,17 +80,22 @@ WSGI_APPLICATION = 'mainapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'restro',
+#         'USER': 'postgres',
+#         'PASSWORD': '12345',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'restro',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 
 
 
--- tableandspace/models.py
+++ tableandspace/models.py
@@ -11,11 +11,20 @@ class TableAndSpace(models.Model):
         OCCUPIED = 'OC', 'Occupied'
         RESERVED = 'RE', 'Reserved'
     
+    class Category(models.TextChoices):
+        SOFA = 'SA', 'Sofa'
+        TABLE = 'SP', 'Space'
+        BOOTH = 'BO', 'Booth'
+        BAR = 'BA', 'Bar'
+        OUTDOOR = 'OU', 'Outdoor'
+        PRIVATE = 'PR', 'Private'
+        
     name = models.CharField(max_length=100)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.AVAILABLE)
     capacity = models.IntegerField()
     location = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-
+    category = models.CharField(max_length=20, choices=Category.choices, default=Category.TABLE)
+    
     def __str__(self):
         return self.name
