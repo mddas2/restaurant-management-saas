26e01eef8e62aa7a771c51fb5ef7455a841f156b
mddas2 <manojdas.py@gmail.com>
2023-10-02 13:47:06 +0545

    order to main

--- orderandbilling/models.py
+++ orderandbilling/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from management.models import FoodItem
-from tableandspace.models import Table
+# from tableandspace.models import Table
+from account.models import CustomUser
+
 
 # Create your models here
 
@@ -14,30 +16,40 @@ class Order(models.Model):
     )
 
     # Basic order information
-    order_number = models.CharField(max_length=20, unique=True)
+    guest_number = models.CharField(max_length=20, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
-    item_name=models.CharField(max_legth=20)
 
-    abstract=True
+    item_id = models.ForeignKey(FoodItem,related_name="order")
+    item_name=models.CharField(max_legth=20)
+    item_price=models.CharField(max_legth=20)
+    item_description=models.CharField(max_legth=20)
+
+    order_type = models.CharField(max_legth=20)
+    table = models.CharField(max_legth=20)
+
     def __str__(self):
         return f"Order #{self.order_number}"
 
     class Meta:
         ordering = ['-created_at']
 
-class InHouseOrder(Order):
+class InHouseOrder(models.Model):
     """ Model for Order in side the Restraunant"""
-    #Table Information
-    table_number=models.ManyToManyField(Table,related_name='inhouseorder')
-    #Food Item Information
-    food_items=models.ManyToManyField(FoodItem,)
 
-class DeliveryOrder(Order):
+    orders=models.ManyToManyField(Order,related_name='inhouseorder')
+    table = models.CharField(max_legth=20)
+    total_price = models.CharField(max_legth=20)
+    status =  models.CharField(max_legth=20)
+   
+
+class DeliveryOrder(models.Model):
     """ Model For Delivery """
-    food_items= models.ManyToManyField(FoodItem,)
-    delivery_address=models.CharField(max_length=50)
+    
+    orders=models.ManyToManyField(Order,related_name='inhouseorder')
+    user=models.ForeignKey(CustomUser,related_name="order_delivery")
+    status =  models.CharField(max_legth=20)
 
 
 # class IndividualTableBill(models.Model):
