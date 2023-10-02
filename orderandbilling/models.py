feca00cf801e9de83ff4b8ed7f806b2705801a0b
mddas2 <manojdas.py@gmail.com>
2023-10-02 14:36:55 +0545

    tenant completed

--- orderandbilling/models.py
+++ orderandbilling/models.py
@@ -21,13 +21,13 @@ class Order(ResturentAwareModel):
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
 
-    item_id = models.ForeignKey(FoodItem,related_name="order")
-    item_name=models.CharField(max_legth=20)
-    item_price=models.CharField(max_legth=20)
-    item_description=models.CharField(max_legth=20)
+    item_id = models.ForeignKey(FoodItem,related_name="order",on_delete=models.CASCADE)
+    item_name=models.CharField(max_length=20)
+    item_price=models.CharField(max_length=20)
+    item_description=models.CharField(max_length=20)
 
-    order_type = models.CharField(max_legth=20)
-    table = models.CharField(max_legth=20)
+    order_type = models.CharField(max_length=20)
+    table = models.CharField(max_length=20)
 
     def __str__(self):
         return f"Order #{self.order_number}"
@@ -39,17 +39,17 @@ class InHouseOrder(ResturentAwareModel):
     """ Model for Order in side the Restraunant"""
 
     orders=models.ManyToManyField(Order,related_name='inhouseorder')
-    table = models.CharField(max_legth=20)
-    total_price = models.CharField(max_legth=20)
-    status =  models.CharField(max_legth=20)
+    table = models.CharField(max_length=20)
+    total_price = models.CharField(max_length=20)
+    status =  models.CharField(max_length=20)
    
 
 class DeliveryOrder(ResturentAwareModel):
     """ Model For Delivery """
     
-    orders=models.ManyToManyField(Order,related_name='inhouseorder')
-    user=models.ForeignKey(CustomUser,related_name="order_delivery")
-    status =  models.CharField(max_legth=20)
+    orders=models.ManyToManyField(Order,related_name='order_delivery')
+    user=models.ForeignKey(CustomUser,related_name="order_delivery",on_delete=models.CASCADE)
+    status =  models.CharField(max_length=20)
 
 
 # class IndividualTableBill(models.Model):
