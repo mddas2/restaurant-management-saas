849197fb09f4cffaa145ebe645db4a0dccd3aaaf
Sgrneane <sgrneaneprof@gmail.com>
2023-10-02 11:59:05 +0545

    Models For Order Created

--- orderandbilling/models.py
+++ orderandbilling/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from management.models import FoodItem
+from tableandspace.models import Table
 
-# Create your models here.
+# Create your models here
+
+class Order(models.Model):
+    """Model for Order """
+    STATUS_CHOICES = (
+        ('Pending', 'Pending'),
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed'),
+        ('Canceled', 'Canceled'),
+    )
+
+    # Basic order information
+    order_number = models.CharField(max_length=20, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+
+    abstract=True
+    def __str__(self):
+        return f"Order #{self.order_number}"
+
+    class Meta:
+        ordering = ['-created_at']
+
+class InHouseOrder(Order):
+    """ Model for Order in side the Restraunant"""
+    #Table Information
+    table_number=models.ManyToManyField(Table,related_name='inhouseorder')
+    #Food Item Information
+    food_items=models.ManyToManyField(FoodItem,)
+
+class DeliveryOrder(Order):
+    """ Model For Delivery """
+    food_items=models.ManyToManyField(FoodItem,)
+    delivery_address=models.CharField(max_length=50)
